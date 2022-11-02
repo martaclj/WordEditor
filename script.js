0ad26ab7fa8699db38852e5f719e4c99bf0dfdfb
martaclj <104948192+martaclj@users.noreply.github.com>
2022-11-02 13:38:01 +0100

    text editor functions

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function setColor() {
   const input = document.querySelectorAll("input")[0];
   el.style.color = input.value;
 };
+function toggleUnderline() {}
+function toggleItalic() {}
+function toggleBold() {}
+function toggleMonospace() {}
+function removeFormat() {}
